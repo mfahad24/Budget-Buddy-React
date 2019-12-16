9584e44d456a6924c089b0964955c3cf6913f1bf
Mohammed Fahad <mohammedfahad@live.com>
2019-12-15 20:41:08 -0500

    homepage mobile complete + navbar mobile started, needs onclick func

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -3,18 +3,29 @@ import "./Navigation.css";
 
 class Navigation extends Component {
   render() {
-    return (
-      <div>
-        <nav className="app-nav">
-          <div className="app-title">Budget Buddy</div>
-          <div className="app-links">
-            <a href="#">Home</a>
-            <a href="#">Let's Budget!</a>
-            <a href="#">History</a>
-          </div>
-        </nav>
-      </div>
-    );
+    if (window.innerWidth > 600) {
+      return (
+        <div>
+          <nav className="app-nav">
+            <div className="app-title">Budget Buddy</div>
+            <div className="app-links">
+              <a href="#">Home</a>
+              <a href="#">Let's Budget!</a>
+              <a href="#">History</a>
+            </div>
+          </nav>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <nav className="app-nav-alt">
+            <i class="app-nav-alt-hamburger align justify icon"></i>
+            <div className="app-title">Budget Buddy</div>
+          </nav>
+        </div>
+      );
+    }
   }
 }
 
