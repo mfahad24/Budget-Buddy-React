3cd8d0826f669e31c689103a33c8001e2f745125
Mohammed Fahad <mohammedfahad@live.com>
2019-12-16 22:22:15 -0500

    a lot of conditional work on nav + styling

--- components/HomePage/HomePage.js
+++ components/HomePage/HomePage.js
@@ -6,22 +6,20 @@ class HomePage extends Component {
     return (
       <div className="app-home-page">
         {/* <div className="app-home-page-box"> */}
-        <p className="app-home-instruction">Please enter your weekly budget</p>
+        <p className="app-home-instruction">Please enter your weekly budget:</p>
         {/* <input className="app-home-input" type="number" placeholder="$"></input> */}
         <div className="app-home-input ui right labeled input">
-          <label for="amount" class="ui label">
+          <label htmlFor="amount" className="ui label">
             $
           </label>
           <input type="text" placeholder="Amount" id="amount"></input>
           <div className="ui basic label">.00</div>
         </div>
         <div className="app-button-container">
-          <button className="app-button-submit ui primary button massive">
+          <button className="app-button-submit ui primary button big">
             Submit
           </button>
-          <button className="app-button-clear ui button massive">
-            Restart
-          </button>
+          <button className="app-button-clear ui button big">Restart</button>
         </div>
 
         {/* </div> */}
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -2,7 +2,34 @@ import React, { Component } from "react";
 import "./Navigation.css";
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { altNavLinks: false };
+  }
+  showNavLinks = () => {
+    console.log("I am first");
+    return this.state.altNavLinks
+      ? this.setState({ altNavLinks: false })
+      : this.setState({ altNavLinks: true });
+  };
+
+  renderNavLinks = () => {
+    console.log("And I am second");
+    if (this.state.altNavLinks) {
+      return (
+        <div className="app-links-alt">
+          <a href="#">Home</a>
+          <a href="#">Let's Budget!</a>
+          <a href="#">History</a>
+        </div>
+      );
+    }
+  };
+
   render() {
+    const isAltNavsActive = this.state.altNavLinks;
+    console.log("is alt nav active", isAltNavsActive);
     if (window.innerWidth > 600) {
       return (
         <div>
@@ -20,8 +47,21 @@ class Navigation extends Component {
       return (
         <div>
           <nav className="app-nav-alt">
-            <i class="app-nav-alt-hamburger align justify icon"></i>
-            <div className="app-title">Budget Buddy</div>
+            <i
+              className="app-nav-alt-hamburger align justify icon"
+              onClick={this.showNavLinks}
+            ></i>
+            <div
+              className={
+                "app-title-alt " +
+                (this.state.altNavLinks
+                  ? "app-nav-title-alt--hide"
+                  : "app-nav-title-alt--show")
+              }
+            >
+              Budget Buddy
+            </div>
+            {this.renderNavLinks()}
           </nav>
         </div>
       );
