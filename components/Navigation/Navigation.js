8aef04269041e7566b95c789e04cbc8b2cfdbc6c
Mohammed Fahad <mohammedfahad@live.com>
2019-12-17 22:20:31 -0500

    initial windowinnerWidth state changed to onMount val, nav icon toggle

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ class Navigation extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { altNavLinks: false, windowWidth: 0 };
+    this.state = { altNavLinks: false, windowWidth: null };
   }
   showNavLinks = () => {
     return this.state.altNavLinks
@@ -25,11 +25,30 @@ class Navigation extends Component {
     }
   };
 
+  renderNavIcon = () => {
+    if (!this.state.altNavLinks) {
+      return (
+        <i
+          className="app-nav-alt-hamburger align justify icon"
+          onClick={this.showNavLinks}
+        ></i>
+      );
+    } else {
+      return (
+        <i
+          className="app-nav-alt-hamburger close icon"
+          onClick={this.showNavLinks}
+        ></i>
+      );
+    }
+  };
+
   listenToResize = () => {
     return this.setState({ windowWidth: window.innerWidth });
   };
 
   componentDidMount() {
+    this.setState({ windowWidth: window.innerWidth });
     window.addEventListener("resize", this.listenToResize);
   }
 
@@ -55,10 +74,7 @@ class Navigation extends Component {
       return (
         <div>
           <nav className="app-nav-alt">
-            <i
-              className="app-nav-alt-hamburger align justify icon"
-              onClick={this.showNavLinks}
-            ></i>
+            {this.renderNavIcon()}
             <div
               className={
                 "app-title-alt " +
