0458d9ea00de75a6e319b2b4d7b4982be8e3470b
Mohammed Fahad <mohammedfahad@live.com>
2020-05-13 23:46:11 -0400

    removed form background gradient styles form now saves and submits values getting error for form expense type new reducer and associated constants/actions might not be necessary

--- src/components/ExpenseEntry/ExpenseEntry.js
+++ src/components/ExpenseEntry/ExpenseEntry.js
@@ -1,33 +1,65 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { subtractFromBudget } from "../HomePage/actions";
+import { subtractFromBudget, weeklyBudget } from "../HomePage/actions";
 import "./ExpenseEntry.css";
 
 class ExpenseEntry extends Component {
+  //reference for the form functions https://reactjs.org/docs/forms.html
   constructor() {
     super();
     this.state = {
-      subtractValue: null,
+      subtractValue: "",
+      subtractValueType: null,
     };
+
+    this.saveExpense = this.saveExpense.bind(this);
+    this.updateStoreBudget = this.updateStoreBudget.bind(this);
   }
-  calculateExpense(val) {
-    console.log(this.state.subtractValue);
+  saveExpense(event) {
+    this.setState({
+      subtractValue: event.target.value,
+      subtractValueType: event.target.subtractValueType,
+    });
+  }
+
+  updateStoreBudget(event) {
+    event.preventDefault();
+    console.log(
+      "values",
+      this.state.subtractValueType,
+      ":",
+      this.state.subtractValue
+    );
   }
   render() {
     return (
       <div className="expense-entry-container">
-        <form className="expense-entry">
-          <input type="text" placeholder="Type of expense" />
+        <form className="expense-entry" onSubmit={this.updateStoreBudget}>
+          {/* <input
+            type="text"
+            placeholder="Type of expense"
+            value={this.state.subtractValueType}
+            onChange={this.saveExpense}
+          /> */}
+          <select
+            value={this.state.subtractValueType}
+            onChange={this.saveExpense}
+          >
+            {/* left off here  */}
+            <option selected value="---">
+              Select the type of expense
+            </option>
+            <option value="food">Food</option>
+            <option value="bills">Bills</option>
+            <option value="shopping">Shopping</option>
+          </select>
           <input
             type="number"
             placeholder="Amount"
             value={this.state.subtractValue}
+            onChange={this.saveExpense}
           />
-          <input
-            type="submit"
-            value="submit"
-            onSubmit={this.calculateExpense()}
-          />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
@@ -40,4 +72,6 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {})(ExpenseEntry);
+export default connect(mapStateToProps, { subtractFromBudget, weeklyBudget })(
+  ExpenseEntry
+);
